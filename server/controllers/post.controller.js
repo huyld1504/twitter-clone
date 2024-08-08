68ed8cc46e945e788563b0390c535066d938d7cb
ledinhhuy <huy@ioz.jp>
2024-08-08 20:25:56 +0700

    added feature s in post controller

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -40,11 +40,11 @@ export const deletePost = async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
         if (!post) {
-            return res.status(404).json({success: false, message: "Post Not Found"});
+            return res.status(404).json({ success: false, message: "Post Not Found" });
         }
 
         if (post.user.toString() !== req.user._id.toString()) {
-            return res.status(401).json({success: false, message: "Unauthorized to access this resource"});
+            return res.status(401).json({ success: false, message: "Unauthorized to access this resource" });
         }
 
         if (post.img) {
@@ -53,10 +53,10 @@ export const deletePost = async (req, res) => {
         }
 
         await Post.findByIdAndDelete(req.params.id);
-        res.status(201).json({success: true, message: "Post deleted successfully"});
+        res.status(201).json({ success: true, message: "Post deleted successfully" });
     } catch (error) {
         console.log(err.message)
-        res.status(500).json({success: false, message: error.message});
+        res.status(500).json({ success: false, message: error.message });
     }
 }
 
@@ -64,11 +64,11 @@ export const commentOnPost = async (req, res) => {
     const result = {
         success: false,
         message: "",
-        data:[]
+        data: []
     };
 
     try {
-        const {text} = req.body;
+        const { text } = req.body;
         const postId = req.params.id;
         const userId = req.user._id;
 
@@ -81,9 +81,9 @@ export const commentOnPost = async (req, res) => {
         if (!post) {
             result.message = "Post not found";
             return res.status(404).json(result);
-        }    
+        }
 
-        const comment = {user: userId, text};
+        const comment = { user: userId, text };
         post.comments.push(comment);
         await post.save();
 
@@ -106,7 +106,7 @@ export const likeUnlikePost = async (req, res) => {
 
     try {
         const userId = req.user._id;
-        const {id: postId} = req.params;
+        const { id: postId } = req.params;
 
         const post = await Post.findById(postId);
         if (!post) {
@@ -118,13 +118,15 @@ export const likeUnlikePost = async (req, res) => {
 
         if (userLikedPost) {
             //Unlike post
-            await Post.updateOne({_id: postId}, {$pull: {likes: userId}});
+            await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
+            await User.updateOne({ _id: userId }, { $pull: { likedPosts: postId } });
             result.success = true;
             result.message = "Unliked post successfully";
             res.status(200).json(result);
         } else {
             //Like post
             post.likes.push(userId);
+            await User.updateOne({ _id: userId }, { $push: { likedPosts: postId } });
             await post.save();
             result.message = "Liked post successfully";
             result.success = true;
@@ -144,3 +146,91 @@ export const likeUnlikePost = async (req, res) => {
         res.status(500).json(result);
     }
 }
+
+export const getAllPosts = async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ createdAt: -1 }).populate({
+            path: "user",
+            select: "-password"
+        }).populate({
+            path: "comments.user",
+            select: "-password"
+        });
+
+        if (posts.length === 0) {
+            return res.status(200).json({ message: "No Posts", data: [] });
+        }
+
+        res.status(200).json(posts)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export const getLikedPost = async (req, res) => {
+    const userId = req.params.id;
+    try {
+        const user = await User.findById(userId);
+        if (!user) return res.status(404).json({ success: false, message: "User Not Found" });
+
+        const likedPosts = await Post.find({ _id: { $in: user.likedPosts } }).populate({
+            path: "user",
+            select: "-password"
+        }).populate({
+            path: "comments.user",
+            select: "-password"
+        });
+
+        res.status(200).json({success: true, data: likedPosts});
+    } catch (error) {
+        console.log(err);
+        res.status(500).json({message: error.message});
+    }
+}
+
+export const getFollowingPosts = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const user = await User.findById(userId);
+        if (!user) return res.status(404).json({ success: false, message: "User Not Found" });
+
+        const following = user.following;
+
+        const feedPosts = await Post.find({user: {$in: following}}).sort({createdAt: -1}).populate({
+            path: "user",
+            select: "-password"
+        }).populate({
+            path: "comments.user",
+            select: "-password"
+        });
+
+        res.status(200).json({success: true, data: feedPosts})
+
+    } catch (error) {
+        console.log(error),
+        res.status(500).json({success: false, message: error.message});
+    }
+}
+
+export const getUserPosts = async (req, res) => {
+    try {
+        const {username} = req.params;
+        const user = await User.findOne({username});
+        if (!user) return res.status(404).json({ success: false, message: "User Not Found" });
+
+        const posts = await Post.find({user: user._id}).sort({createdAt: -1}).populate({
+            path: "user",
+            select: "-password"
+        }).populate({
+            path: "comments.user",
+            select: "-password"
+        })
+
+        res.status(200).json({success: true, data: posts});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({success: false, message: error.message});
+    }
+}
+
--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { protectRoute } from "../middlewares/protectRoute.js";
-import { createPost, deletePost, commentOnPost, likeUnlikePost } from "../controllers/post.controller.js";
+import { createPost, deletePost, commentOnPost, likeUnlikePost, getAllPosts, getLikedPost, getFollowingPosts, getUserPosts } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
+router.get("/all", protectRoute, getAllPosts);
+router.get("/likes/:id", protectRoute, getLikedPost);
+router.get("/following", protectRoute, getFollowingPosts);
+router.get("/user/:username", protectRoute, getUserPosts);
 router.post("/create", protectRoute, createPost);
 router.delete("/delete/:id", protectRoute, deletePost);
 //router.post("/update/:id", protectRoute, updatePost);
