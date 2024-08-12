561b965c012ab4e4b81b8ddab701517f9e18042d
ledinhhuy <huy@ioz.jp>
2024-08-12 08:45:47 +0700

    config vercel

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(express.json({limit: "5mb"})); //to parse req.body DoS attack
 app.use(express.urlencoded({extended: true})); // to parse form data
 
 app.use(cookieParser());
-// app.use(cors());
+app.use(cors());
 
 
 app.use("/api/auth", authRoutes);
